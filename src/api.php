5bdf7a5530d611348d4caa345ab5b708c532c2aa
alextselegidis <alextselegidis@gmail.com>
2016-08-22 21:29:57 +0200

    Added file banner to api.php

--- src/api.php
+++ src/api.php
@@ -1,1 +1,24 @@
 <?php
+
+/* ----------------------------------------------------------------------------
+ * Riddles4u - Amazing Riddles Game Platform
+ *
+ * @package     Riddles4u
+ * @author      Alex Tselegidis <alextselegidis@gmail.com>
+ * @copyright   Copyright (c) 2016, BigBlackCode
+ * @license     http://opensource.org/licenses/GPL-3.0 - GPLv3
+ * @link        http://riddles4u.com
+ * @since       v1.0.0
+ * ---------------------------------------------------------------------------- */
+
+/**
+ * Backend API Access File
+ *
+ * Supported Operations:
+ *
+ * - Get Riddle Question
+ * - Check Riddle Answer
+ * - Get Ad Image
+ * - Redirect To Ad URL
+ */
+
