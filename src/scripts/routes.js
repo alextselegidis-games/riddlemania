e981dc87f2d792761fc0a82654127e900d1ce92c
alextselegidis <alextselegidis@gmail.com>
2016-09-04 13:40:40 +0200

    Created base files for the frontend files.

---
+++ src/scripts/routes.js
@@ -0,0 +1,24 @@
+/* ----------------------------------------------------------------------------
+ * Riddles4U - Amazing Riddles Game Platform
+ *
+ * @package     Riddles4U
+ * @author      Alex Tselegidis <alextselegidis@gmail.com>
+ * @copyright   Copyright (c) 2016, BigBlackCode
+ * @license     http://opensource.org/licenses/GPL-3.0 - GPLv3
+ * @link        http://riddles4u.com
+ * @since       v1.0.0
+ * ---------------------------------------------------------------------------- */
+
+import page from 'page';
+
+const routes = [
+    () => {
+
+}
+];
+
+export default function registerRoutes() {
+    for (route of routes) {
+        route();
+    }
+}
