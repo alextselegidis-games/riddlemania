38708fa38b8624b8baa385f6f6acec3e0b1efbb9
alext <alextselegidis@gmail.com>
2016-12-30 16:59:00 +0200

    Created Input class.

---
+++ src/system/Input.php
@@ -0,0 +1,46 @@
+<?php
+
+/* ----------------------------------------------------------------------------
+ * Riddles4U - Amazing Riddles Game Platform
+ *
+ * @package     Riddles4U
+ * @author      Alex Tselegidis <alextselegidis@gmail.com>
+ * @copyright   Copyright (c) 2016, BigBlackCode
+ * @license     http://opensource.org/licenses/GPL-3.0 - GPLv3
+ * @link        http://riddles4u.com
+ * @since       v1.0.0
+ * ---------------------------------------------------------------------------- */
+
+namespace Riddles4U\System;
+
+/**
+ * Class Input
+ *
+ * Handles request input parameter and body values.
+ *
+ * @package Riddles4U\System
+ */
+class Input
+{
+    /**
+     * Get the value of a GET parameter.
+     *
+     * @param string $key The key of the parameter to be returned.
+     *
+     * @return string|null Returns the parameter value.
+     */
+    public function get($key)
+    {
+        return array_key_exists($key, $_GET) ? $_GET[$key] : null;
+    }
+
+    /**
+     * Get the decoded request JSON body as an associative array.
+     *
+     * @return array Returns an associative array of the request JSON body.
+     */
+    public function getBody()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+}
